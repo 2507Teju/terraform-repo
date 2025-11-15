1dfc3905bbc01f6dd05625571be6cc74ec38884d
2507Teju <tgowdatejashwini@gmail.com>
2025-11-16 00:24:30 +0530

    code added

---
+++ main.tf
@@ -0,0 +1,41 @@
+provider "aws"{
+    region = "ap-south-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket  = aws_s3.terraform_backend.bucket
+    key     = "atlantis/terraform.tfstate"
+    region  = "us-east-1"
+    profile = "default"
+  }
+}
+
+resource "aws_s3" "terraform_backend"{
+    bucket = "test-atlantis"
+}
+################# EC2 INSTANCE CREATION #########
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-atlantis"
+  }
+}
